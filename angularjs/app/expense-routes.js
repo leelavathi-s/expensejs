9f2ddc2c8d317d6f2ab57a3b256106a62a479c47
Leela Arumugam <leelaintce@gmail.com>
2018-02-19 15:15:26 +0530

    Updates

--- angularjs/app/expense-routes.js
+++ angularjs/app/expense-routes.js
@@ -1,14 +1,16 @@
 var app = angular
-.module("expenseTracker")
-.config(function($routeProvider){
- $routeProvider.when("/expense",{
-   templateUrl:"expense/expense.html"
- })
- .when("/category",{
-   templateUrl:"category/category.html"
- })
- .when("/home",{
-   templateUrl:"/home.html"
- });
-});
+  .module("expenseTracker")
+  .config(function ($routeProvider) {
+    $routeProvider.when("/expense", {
+      templateUrl: "expense/expense.html"
+    })
+      .when("/category", {
+        templateUrl: "category/category.html"
+      })
+      .when("/home", {
+        templateUrl: "/home.html"
+      }).otherwise("/", {
+        templateUrl: "/home.html"
+      });
+  });
 
